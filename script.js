330291f99315d0fdb82894d2c147aeee22424c04
VladimirTi <tytmes@mail.ru>
2019-05-22 10:42:01 +0300

    correction

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 function createCalendar(ID, year, month) {
   let html = '<tr><th>ПН</th><th>ВТ</th><th>СР</th><th>ЧТ</th><th>ПТ</th><th>СБ</th><th>ВС</th></tr>'
-  let temp = '';
+  let dayHTML = '';
   let date = new Date(year, month - 1);
 
-  let firstDay = date.getDay() === 0 ? 7 : date.getDay();
+  let firstDay = date.getDay()
+  if (firstDay === 0) firstDay = 7;
+  
   for(let i = 1; i < firstDay; i++) {
-    temp += `<td></td>`
+    dayHTML += `<td></td>`
   }
 
   while(date.getMonth() + 1 === month) {
-    const firstDayToAdd = date.getDay() === 0 ? 7 : date.getDay()
+    const currentDay = date.getDay()
+    const firstDayToAdd = currentDay === 0 ? 7 : currentDay
     for (let i = firstDayToAdd; i < 8 && date.getMonth() + 1 === month; i++) {
-      temp += `<td>${date.getDate()}</td>`;
-      date.setDate(date.getDate() + 1)
-    }
-    //console.log(date.getDay())
-    while(date.getDay() !== 1) {
-      temp += `<td></td>`
+      dayHTML += `<td>${date.getDate()}</td>`;
       date.setDate(date.getDate() + 1)
     }
 
-    temp = `<tr>${temp}</tr>`
-    html += temp
-    temp = ''
+    while(currentDay !== 1) {
+      dayHTML += `<td></td>`
+      date.setDate(date.getDate() + 1)
+    }
+
+    dayHTML = `<tr>${dayHTML}</tr>`;
+    html += dayHTML;
+    dayHTML = '';
   }
 
-  html = `<table>${html}</table>`
-  document.getElementById(ID).innerHTML = html
+  html = `<table>${html}</table>`;
+  document.getElementById(ID).innerHTML = html;
 }
 
-createCalendar('calendar', 2018, 7)
+createCalendar('calendar', 2018, 7);
